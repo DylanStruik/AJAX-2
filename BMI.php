38f5e1eef24ccd6f933b41e8530954470488c04b
DylanStruik <dylan.struik@outlook.com>
2017-05-30 20:51:59 +0200

    V1.0

---
+++ BMI.php
@@ -0,0 +1,25 @@
+<?php
+
+$gewicht = $_GET['gewicht'];
+$lengte = $_GET['lengte_cm'] / 100;
+// BMIcalc($gewicht,$lengte);
+
+$BMI = $gewicht / ($lengte * $lengte);
+
+
+if($BMI < 18.5){
+echo '<br>Je hebt ondergewicht.';
+}elseif($BMI < 25){
+echo '<br>Je hebt een normaal gewicht.';
+}elseif($BMI < 30){
+echo '<br>Je hebt matig overgewicht.';
+}elseif($BMI < 40){
+echo '<br>Je hebt obesitas';
+}else{
+echo '<br>Je hebt morbide obesitas.';
+}
+
+echo "<br>Uw BMI: " . $BMI;
+
+
+?>
